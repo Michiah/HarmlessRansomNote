960bd83a6c627c6f8c87cf2f84b09d86f8b936e4
Michiah <85957194+Michiah@users.noreply.github.com>
2021-11-10 15:13:53 -0500

    .......

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+    public Boolean harmlessRansomNote(String message , String magazineText) {
+        Map<RansomNote, String> ransomNote = new HashMap();
+        String [] note = null;
+        for (String words: note)
+            if(ransomNote.containsKey(words))
+
+
+
+            return null;
+        }
     }
 }
+//Help...... me
